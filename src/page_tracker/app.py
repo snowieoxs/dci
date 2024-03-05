0f9fa994a5d00838fe1f6b98121b7f34b2a7a148
snowieoxs <snowieoxs@gmail.com>
2024-03-05 09:08:23 -0600

    moved Redis client creation from the *global scope* to a new redis() function, which the controller function calls at runtime on each incoming request.

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,12 +1,17 @@
 # src/page_tracker/app.py
 
+from functools import cache
+
 from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis()
 
 @app.get("/")
 def index():
-    page_views = redis.incr("page_views")
+    page_views = redis().incr("page_views")
     return f"This page has been seen {page_views} times."
+
+@cache
+def redis():
+    return Redis()
