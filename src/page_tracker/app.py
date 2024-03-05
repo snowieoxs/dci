c0c9034e2f277029270684bd130731583a6903a9
snowieoxs <snowieoxs@gmail.com>
2024-03-05 08:45:41 -0600

    Implemented and Ran the Flask Application Locally

---
+++ src/page_tracker/app.py
@@ -0,0 +1,12 @@
+# src/page_tracker/app.py
+
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis()
+
+@app.get("/")
+def index():
+    page_views = redis.incr("page_views")
+    return f"This page has been seen {page_views} times."
